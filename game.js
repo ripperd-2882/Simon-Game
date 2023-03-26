b92952b465b4e70e6a437ed961940076cc4bdac4
ripperd-2882 <ripperd2882@gmail.com>
2023-03-26 23:52:07 +0530

    added game over

--- game.js
+++ game.js
@@ -71,6 +71,10 @@ function checkAnswer(currentLevel) {
   } else {
 
     console.log("wrong");
-
+    var audio=new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){$("body").removeClass("game-over");},200)
+    $("#level-title").text("Game Over, Press Any Key to Restart");
   }
 }
